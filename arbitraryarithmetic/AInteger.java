96b2a1e819b22a7a3f36641b5caf6122eaa9b237
Aryan Agarwal <aryanagarwal@Aryans-MacBook-Air.local>
2025-04-24 14:53:44 +0530

    feat:Contructor for AInteger and basic add function, function to remove zeros at start of string

--- arbitraryarithmetic/AInteger.java
+++ arbitraryarithmetic/AInteger.java
@@ -1,4 +1,93 @@
 package arbitraryarithmetic;
 public class AInteger {
+    //Defining member variables of string and sign of input
+    public String num;//number without sign.
+    public boolean sign;//true if negative
+
+    //AInteger Contructorfor Defualt value
+    public AInteger() {
+        this.num = "0";
+        this.sign = false;
+    }
+
+    // Contructor for string to AInteger
+    public AInteger(String a) {
+        parse(a);       //parsing to AInteger
+    }
     
+    //Parse function
+    private AInteger parse(String a){
+        if (a.charAt(0) == '-') {
+            this.sign = true;
+            String x = "";
+            for (int j = 0; j <= a.length() - 2; j++) {
+                x += a.charAt(j + 1);
+            }
+            a = x;
+        }
+        this.num = removezero(a);
+
+        if (this.num.length() == 0) {
+            this.num = "0";
+        }
+        AInteger ans=new AInteger();
+        ans.num=this.num;
+        ans.sign=this.sign;
+        return ans;
+    }
+
+    //Contructor to assign a AInteger using other predefined.
+    public AInteger(AInteger a) {
+        this.num = a.num;
+        this.sign = a.sign;
+    }
+
+    //Addition Logic
+    public AInteger add(AInteger a) {
+        if (this.sign && a.sign) {
+            return new AInteger("-" + adding(a));
+        }
+        if (this.sign && !a.sign) {
+            return new AInteger(a.subing(this));
+        }
+        if (!this.sign && a.sign) {
+            return new AInteger(this.subing(a));
+        }
+        return new AInteger(adding(a));
+    }
+
+    //Adding the AInteger values only when both the strings are positive
+    public AInteger adding(AInteger a) {
+        String x = a.num;
+        String y = this.num;
+        AInteger ans = new AInteger();
+        ans.num = addstr(x, y);
+        ans.sign = false;
+        return ans;
+    }
+
+    public AInteger subing(AInteger a) {
+
+    }
+
+    private static String addstr(String a,String b) {
+
+    }
+
+    
+    //Removinf zeros at start of string if any
+    private static String removezero(String s) {
+        int i = 0;
+        //break when first non zero element is encountered
+        while (s.charAt(i) == '0' && i < s.length() - 1) 
+        {
+            i++; //Incrementing till the char is 0 from right hand side.
+        }
+
+        String num = "";//output substring
+        for (int j = 0; j < s.length() - i; j++) {
+            num += s.charAt(i + j);//adding all the elements after first non zero element
+        }
+        return num;//return substring
+    }
 }
--- arbitraryarithmetic/MyInfArith.java
+++ arbitraryarithmetic/MyInfArith.java
@@ -2,21 +2,27 @@ package arbitraryarithmetic;
 
 public class MyInfArith {
     public static void main(String[] args) {
+        //If the number of input arguments is less than 4 exit.
         if (args.length < 4) {
             System.out.println("incorrect input1");
             return;
         }
-
+        //Taking inputs from the command line.
         String ds = args[0];
         String opr = args[1];
         String s1 = args[2];
         String s2 = args[3];
 
+        //Creating if for integer numbers
         if (ds.equals("int")) {
 
-        } else if (ds.equals("float")) {
+        } 
+        //Creating if for floating point numbers
+        else if (ds.equals("float")) {
 
-        } else {
+        } 
+        //Incirrect input if none is encountered.
+        else {
             System.out.println("incorrect input");
         }
     }
